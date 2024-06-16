ee7a6649848d8f397aa89502b907a244c7232418
ridhimagu <ridhima1623@gmail.com>
2024-06-16 13:01:48 +0530

    Complaint changed according to depts

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,1 +1,54 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
+@NgModule({
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatIconModule,
+    MatInputModule,
+    MatSelectModule,
+    MatAutocompleteModule,
+    MatFormFieldModule,
+    MatRadioModule,
+    MatDialogModule,
+    MatSnackBarModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatTooltipModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatIconModule,
+    MatInputModule,
+    MatSelectModule,
+    MatAutocompleteModule,
+    MatFormFieldModule,
+    MatRadioModule,
+    MatDialogModule,
+    MatSnackBarModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatTooltipModule
+  ],
+  declarations: []
+})
+export class MaterialModule { }
