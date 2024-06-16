6c5c223aa4843a6d839f174d6319f2e3810d607b
ridhimagu <ridhima1623@gmail.com>
2024-06-16 13:05:28 +0530

    Add routing

---
+++ src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { WebService } from './web.service';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from '../app/app-routing/app-routing.module';
+import { MaterialModule } from './material/material.module';
+import 'hammerjs';
+import { AppComponent } from './app.component';
+import { ComplaintsComponent } from './complaints/complaints.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { HttpModule } from '@angular/http';
+import { AuthService } from './auth.service';
+import { AuthService1 } from './auth2.service';
+import {routingComponents}  from './app-routing/app-routing.module';
+import { LogindepComponent } from './logindep/logindep.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    ComplaintsComponent,
+    FooterComponent,
+    HeaderComponent,
+    routingComponents,
+    LogindepComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FlexLayoutModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MaterialModule,
+    HttpModule,
+  ],
+  providers: [WebService, AuthService, AuthService1 ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
