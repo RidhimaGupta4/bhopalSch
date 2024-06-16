40e1151ca026c34235893569734875e14599de5d
ridhimagu <ridhima1623@gmail.com>
2024-06-16 13:06:39 +0530

    Added Login And register

---
+++ src/app/web.service.ts
@@ -0,0 +1,23 @@
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
+import { Injectable } from '@angular/core';
+
+
+@Injectable()
+export class WebService {
+
+    BASE_URL = 'http://localhost:1234/api';
+    constructor(private http: Http) {
+
+    }
+    getMessages() {
+        try {
+            return this.http.get(this.BASE_URL + '/complaints').toPromise();
+        } catch (error) {
+            console.error('Unable to get messages');
+        }
+    }
+    postMessages(message) {
+        return this.http.post(this.BASE_URL + '/complaints', message).toPromise();
+    }
+}
