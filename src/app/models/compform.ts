dbcb9e15829b4c0629211c5b7496f4f7a9af147a
ridhimagu <ridhima1623@gmail.com>
2024-06-16 13:02:41 +0530

    updated home at college(still working)

--- src/app/models/compform.ts
+++ src/app/models/compform.ts
@@ -1,1 +1,8 @@
-
+export class Compform {
+  title: string;
+  department: string;
+  description: string;
+  date: string;
+  status: string;
+  id: number;
+}
---
+++ src/app/models/complaints.ts
@@ -0,0 +1,8 @@
+export class Complaint {
+    title: string;
+    department: string;
+    description: string;
+    date: string;
+    status: string;
+    id: number;
+}
